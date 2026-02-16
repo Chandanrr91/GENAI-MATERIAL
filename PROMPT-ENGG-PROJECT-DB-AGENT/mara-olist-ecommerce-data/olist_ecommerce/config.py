9abf20ee7be018d9e8f93fb2f627d0aab7c4dff4
shahan shaik <shahan.shaik@dnata.com>
2026-02-16 10:49:33 +0400

    Fixed broken submodule and converted to normal directory

---
+++ PROMPT-ENGG-PROJECT-DB-AGENT/mara-olist-ecommerce-data/olist_ecommerce/config.py
@@ -0,0 +1,34 @@
+"""Configuration of loading olist e-commerce dataset to Postgres"""
+
+import pathlib
+
+
+def data_dir() -> str:
+    """The directory where the csv files are located"""
+    return pathlib.Path(__file__).parent.parent/'data'
+
+
+def db_name() -> str:
+    """Database name"""
+    return 'olist_ecommerce'
+
+
+def db_user() -> str:
+    """Database user"""
+    return 'postgres'
+
+
+def db_password() -> str:
+    """Database password"""
+    return 'admin123'
+
+
+def db_host() -> str:
+    """Database host"""
+    return 'localhost'
+
+
+def db_port() -> str:
+    """Database port"""
+    return '5432'
+
